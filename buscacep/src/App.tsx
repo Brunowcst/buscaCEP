52946176118f7d9256aad0f284684f708bde0b80
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-22 18:46:22 -0300

    Chama rotas

--- buscacep/src/App.tsx
+++ buscacep/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <p>Teste</p>
-    </div>
+    <Routes/>
   );
 }
 
