6766104b73d0fac21050fd1b38f76eda1e0688b6
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-22 23:02:14 -0300

    Página Home com form

--- buscacep/src/pages/home/Home.tsx
+++ buscacep/src/pages/home/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Home.module.css'
+import ModelForm from '../../components/form/ModelForm';
 
 function Home() {
     return (
-        <div>
-            <p>Buscador de cpf</p>
+        <div className={styles.mainContainer}>
+            <h2>Buscador de CEP</h2>
+            <div>
+                <ModelForm/>
+            </div>
         </div>
     );
 }
