02104925c9bd52e8e19778b5d95f7e0c14fb3cff
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-23 12:50:55 -0300

    SideBar responsiva

--- buscacep/src/layout/MainLayout/MainLayout.tsx
+++ buscacep/src/layout/MainLayout/MainLayout.tsx
@@ -1,24 +1,46 @@
-import { Layout } from 'antd';
+import { Layout, Drawer, Button } from 'antd';
 import { Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.css'
 import SideBarMenu from '../../components/SideBar/SideBarMenu';
+import { useState } from 'react';
+import { MenuOutlined } from '@ant-design/icons';
+import { SideBarMenuDrawer } from '../../components/SideBar/SideBarMenu';
 
 function MainLayout() {
-  const sidebarVisible = true;
+  const [sidebarVisible] = useState(true);
   const {Header, Content, Footer} = Layout;
+  const [open, setOpen] = useState(false)
+
 
   return (
-    <Layout>
+    <Layout className={styles.layout}>
       <Header className={styles.header}>
         <div>Imagem</div>
-        <p>Busca CEP</p>
+        <Button
+          className={styles.menuButton}
+          icon={<MenuOutlined />}
+          onClick={() => setOpen(true)}
+        />
       </Header>
       <Layout>
-        <SideBarMenu visible={sidebarVisible}/>
+        <SideBarMenu visible={sidebarVisible} />
+        <Drawer 
+        placement='left'
+        width={200}
+        bodyStyle={{backgroundColor: '#05161a'}} 
+        open={open}
+        onClose={() => 
+          setOpen(false)
+        }
+        closable={false}>
+          <SideBarMenuDrawer/>
+        </Drawer>
 
-        <Content className={styles.content}>
-          <Outlet/>
-        </Content>
+        <Layout className={styles.contentLayout}>
+          <Content className={styles.content}>
+            <Outlet/>
+          </Content>
+        </Layout>
 
       </Layout>
       <Footer className={styles.footer}>Footer</Footer>
