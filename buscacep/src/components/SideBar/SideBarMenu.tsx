3f75e52e63d83122446b9b5f57ca4f5bbf6f23f9
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-07-23 12:50:47 -0300

    Nova instância da sidebar

--- buscacep/src/components/SideBar/SideBarMenu.tsx
+++ buscacep/src/components/SideBar/SideBarMenu.tsx
@@ -9,31 +9,32 @@ interface SideBarMenuProps {
   visible: boolean;
 }
 
+const menuItems = [
+  { key: 'home', label: 'Home', route: '/buscaCEP', icon: <HomeOutlined /> },
+  { key: 'contacts', label: 'Contacts', route: '/contacts', icon: <UserOutlined /> },
+  { key: 'about', label: 'Sobre', route: '/about', icon: <InfoCircleOutlined /> },
+];
+
+const { Sider } = Layout;
+const { Item } = Menu;
+
 function SideBarMenu({ visible }: SideBarMenuProps) {
   const [collapsed, setCollapsed] = useState(true);
-  const { Sider } = Layout;
-  const { Item } = Menu;
-
-  const menuItems = [
-    { key: 'home', label: 'Home', route: '/buscaCEP', icon: <HomeOutlined /> },
-    { key: 'contacts', label: 'Contacts', route: '/contacts', icon: <UserOutlined /> },
-    { key: 'about', label: 'Sobre', route: '/about', icon: <InfoCircleOutlined /> },
-  ];
-
- 
-
+  
   const toggleMenu = (visible: boolean) => {
     setCollapsed(!visible);
   };
 
   return (
     <Sider
+    className={styles.sider}
       trigger={null}
       collapsible
       collapsed={collapsed}
       onMouseEnter={() => toggleMenu(true)}
       onMouseLeave={() => toggleMenu(false)}
       style={{ background: '#05161a' }}
+      collapsedWidth={45}
     >
       <Menu className={styles.menu} theme="dark" mode="vertical" defaultSelectedKeys={['home']}>
         {menuItems.map((item) => (
@@ -50,3 +51,27 @@ function SideBarMenu({ visible }: SideBarMenuProps) {
 }
 
 export default SideBarMenu;
+
+
+export function SideBarMenuDrawer() {
+
+  
+  return (
+    <Sider
+    className={styles.siderDrawer}
+      trigger={null}
+      collapsible
+      style={{ background: '#05161a' }}
+    >
+      <Menu className={styles.menu} theme="dark" mode="vertical" defaultSelectedKeys={['home']}>
+        {menuItems.map((item) => (
+          <Item key={item.key}>
+            <NavLink to={item.route}>
+              {item.icon} {item.label}
+            </NavLink>
+          </Item>
+        ))}
+      </Menu>
+    </Sider>
+  );
+}
