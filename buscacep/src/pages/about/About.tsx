d347831d6f7349f94a68fca5a4ee634195ed9c1c
Brunowcst <brunoporfirio022@gmail.com>
2023-08-16 23:08:17 -0300

    Adiciona conteúdo página About

--- buscacep/src/pages/about/About.tsx
+++ buscacep/src/pages/about/About.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import styles from './About.module.css';
 
 function About() {
     return (
-        <div>
-            Sobre
+        <div className={styles.content}>
+            <p>Este projeto tem o objetivo principal de praticar o uso do pacote <a href="https://ant.design/" target="_blank" rel="noreferrer external">Ant-Desing.</a></p>
+
+            <p>O Ant Design React é uma biblioteca que oferece um conjunto abrangente de componentes e ferramentas para ajudar os desenvolvedores a criarem interfaces incríveis e responsivas em seus projetos ReactJS.</p>
         </div>
     );
 }
